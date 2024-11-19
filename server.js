350812919902232000562e573efa72708b895eb0
DigSix <thediogoviana@gmail.com>
2024-11-18 21:53:20 -0300

    Reducing codes

--- server.js
+++ server.js
@@ -1,30 +1,20 @@
-const net = require('node:net');
-const fs = require('fs'); // Módulo para manipulação de arquivos
+const net = require("node:net");
+const fs = require("fs"); // Módulo para manipulação de arquivos
 
 const server = net.createServer((socket) => {
     console.log("Socket connected!");
 
-    socket.on('data', (data) => {
-        // Analisa a primeira linha da requisição para obter o caminho do arquivo
-        const request = data.toString('utf-8').split('\r\n')[0];
-        const [method, url] = request.split(' ');
+    socket.on("data", (data) => {
+        // Analisa a primeira linha da requisição e realiza o split "GET / HTTP/1.1 --> method, path, protocol"
+        const request = data.toString("utf-8").split("\r\n")[0];
+        const [method, path, protocol] = request.split(" ");
 
-        console.log(`Received request: ${method} ${url}`);
+        console.log(`Received request: ${method} ${path} ${protocol}`);
 
-        // Determina o arquivo a ser servido com base na URL
-        let filePath = './index.html';
-        let contentType = 'text/html';
-
-        if (url === '/styles.css') {
-            filePath = './styles.css';
-            contentType = 'text/css';
-        } else if (url === '/script.js') {  // Novo bloco para servir o script.js
-            filePath = './script.js';
-            contentType = 'application/javascript';
-        }
+        let filePath = path === "/" ? "./index.html" : "." + path;
 
         // Lê o arquivo e envia a resposta
-        fs.readFile(filePath, 'utf8', (err, content) => {
+        fs.readFile(filePath, "utf8", (err, content) => {
             if (err) {
                 // Em caso de erro ao ler o arquivo, enviar uma resposta de erro
                 socket.write("HTTP/1.1 500 Internal Server Error\r\n\r\n");
@@ -35,8 +25,7 @@ const server = net.createServer((socket) => {
             // Construindo a resposta HTTP com o conteúdo do arquivo
             const httpResponse =
                 "HTTP/1.1 200 OK\r\n" +
-                `Content-Type: ${contentType}\r\n` +
-                `Content-Length: ${Buffer.byteLength(content, 'utf8')}\r\n` +
+                `Content-Length: ${Buffer.byteLength(content, "utf8")}\r\n` +
                 "\r\n" +
                 content;
 
